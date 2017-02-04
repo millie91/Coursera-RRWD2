30ca4af4821565d219522f5fe8927c1f473f2065
Milica Sucevic <milicasucevic@gmail.com>
2017-02-04 11:59:30 +0000

    Code Updated

--- fancy_cars/db/schema.rb
+++ fancy_cars/db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170204102347) do
+ActiveRecord::Schema.define(version: 20170204114045) do
 
   create_table "cars", force: :cascade do |t|
-    t.string   "make"
+    t.string   "company"
     t.string   "color"
     t.integer  "year"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.decimal  "price",      precision: 10, scale: 2
   end
 
 end
