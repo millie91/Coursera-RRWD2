1516db5d1d6a7df62afc219ccbb759572d4180bd
Milica Sucevic <milicasucevic@gmail.com>
2017-02-05 12:06:06 +0000

    Code Updated

--- fancy_cars/db/schema.rb
+++ fancy_cars/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170204114045) do
+ActiveRecord::Schema.define(version: 20170205115105) do
 
   create_table "cars", force: :cascade do |t|
     t.string   "company"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20170204114045) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.decimal  "price",      precision: 10, scale: 2
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
