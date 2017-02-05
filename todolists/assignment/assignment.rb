026084d8c0589f9bed91a096410ff803210d439f
Milica Sucevic <milicasucevic@gmail.com>
2017-02-05 15:38:07 +0000

    Insert Rows in DB

--- todolists/assignment/assignment.rb
+++ todolists/assignment/assignment.rb
@@ -17,6 +17,7 @@ class Assignment
       # accept a hash of todolist properties (`:name` and `:due_date`) as an input parameter. Note these are not 100% the same as Model class.
       # use the TodoList Model class to create a new user in the DB
       # return an instance of the class with primary key (`id`), and dates (`created_at` and `updated_at`) assigned
+      TodoList.create(:list_name => params[:name], :list_due_date => params[:due_date])
   end
 
   #
