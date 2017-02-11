661905d2094c5e6b22a18b1f7df95bb3747215fc
Milica Sucevic <milicasucevic@gmail.com>
2017-02-11 17:04:26 +0000

    Code Updated

--- advanced_ar/app/models/person.rb
+++ advanced_ar/app/models/person.rb
@@ -8,4 +8,7 @@ class Person < ActiveRecord::Base
 	def max_salary
       approx_salaries.maximum(:max_salary)
 	end
+
+	scope :ordered_by_age, -> { order age: :desc}
+	scope :starts_with, -> (starting_string) {where("first_name LIKE ?", "#{starting_string}%")}
 end
