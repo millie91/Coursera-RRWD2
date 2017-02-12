58c1fbb408efce1c7fc060f04cb5217e7ba76f80
Milica Sucevic <milicasucevic@gmail.com>
2017-02-12 11:52:19 +0000

    Create TodoList Model Class

--- ActiveRecordRelationships/todolists/db/schema.rb
+++ ActiveRecordRelationships/todolists/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170212112604) do
+ActiveRecord::Schema.define(version: 20170212114947) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 20170212112604) do
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
+
+  create_table "todo_lists", force: :cascade do |t|
+    t.string   "list_name"
+    t.date     "list_due_date"
+    t.integer  "user_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "todo_lists", ["user_id"], name: "index_todo_lists_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
