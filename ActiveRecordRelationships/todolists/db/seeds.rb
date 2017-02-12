76e8a095e7e2b99cc1df3937f00ca4f72ac04f9b
Milica Sucevic <milicasucevic@gmail.com>
2017-02-12 13:21:57 +0000

    Create Seeds.db

--- ActiveRecordRelationships/todolists/db/seeds.rb
+++ ActiveRecordRelationships/todolists/db/seeds.rb
@@ -6,6 +6,32 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
 User.destroy_all
 Profile.destroy_all
 TodoList.destroy_all
+TodoItem.destroy_all
+
+due_date = Date.today + 1.year
+
+Profile.create! [
+  { first_name: "Carly", last_name: "Fiorina", gender: "female" ,birth_year: 1954, },
+  { first_name: "Donald", last_name: "Trump", gender: "male" ,birth_year: 1946, },
+  { first_name: "Ben", last_name: "Carson", gender: "male" ,birth_year: 1951, },
+  { first_name: "Hillary", last_name: "Clinton", gender: "female" ,birth_year: 1947, }
+]
+
+profiles = Profile.all
+
+profiles.each do |profile|
+  profile.create_user( username: profile.last_name, password_digest: profile.birth_year )
+  profile.user.todo_lists.create!(list_name: profile.first_name, list_due_date: due_date )
+end
+
+todolists = TodoList.all
+
+todolists.each do |list|
+  5.times do
+  list.todo_items.create!(due_date: due_date, title: "Title", description: "Description")
+  end
+end
