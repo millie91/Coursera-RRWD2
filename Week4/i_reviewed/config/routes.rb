c6470f973d69cbe86f61cc8dbc1f8577c52e9ddf
Milica Sucevic <milicasucevic@gmail.com>
2017-02-25 10:43:49 +0000

    Using Before_action

--- Week4/i_reviewed/config/routes.rb
+++ Week4/i_reviewed/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :books
+  resources :books do
+    resources :notes, only: [:create, :destroy]
+  end
+
   root to: "books#index"
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,4 +59,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
