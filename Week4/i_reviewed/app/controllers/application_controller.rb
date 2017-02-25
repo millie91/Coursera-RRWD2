2687e645c4da70bca28661d35eda8d3b539e1711
Milica Sucevic <milicasucevic@gmail.com>
2017-02-25 14:22:57 +0000

    Authorization

--- Week4/i_reviewed/app/controllers/application_controller.rb
+++ Week4/i_reviewed/app/controllers/application_controller.rb
@@ -2,4 +2,32 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :ensure_login
+  helper_method :logged_in?, :current_user
+  # by setting these as helper methods like this they are available not just
+  # to the controllers due to the other controllers inheriting from ApplicationController
+  # but to the views as well. Different from the view helpers defined in
+  # /helpers
+
+  # how we lock down the app so the pages aren't accessible until you Login
+  protected
+  # unlike private, this will allow the other controllers to inherit
+    def ensure_login
+      # always go to login page unless session contains reviewer_id
+      redirect_to login_path unless session[:reviewer_id]
+    end
+
+    def logged_in?
+      session[:reviewer_id] # nil is false
+    end
+
+    def current_user
+      @current_user ||= Reviewer.find session[:reviewer_id]
+      # caches current_user instance variable for this particular request, not
+      # in between requests
+      # current_user is an instance variable of the ApplicationController which
+      # the other controllers inherit from. also accessible in views. called
+      # simply with current_user
+    end
 end
--- Week4/i_reviewed/db/seeds.rb
+++ Week4/i_reviewed/db/seeds.rb
@@ -1,33 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 Book.destroy_all
+Reviewer.destroy_all
 
 Book.create! [
- { name: "Elouquent Ruby", author: "Russ Olsen" },
- { name: "Beginning Ruby", author: "Peter Cooper" },
- { name: "Metaprogramming Ruby 2", author: "Paolo Perotta" },
- { name: "Design Patterns in Ruby", author: "Russ Olsen" },
- { name: "The Ruby Programming Language", author: "David Flanagan" },
+  { name: "Eloquent Ruby", author: "Russ Olsen" },
+  { name: "Beginning Ruby", author: "Peter Cooper" },
+  { name: "Metaprogramming Ruby 2", author: "Pailo Perrotta" },
+  { name: "Design Patterns in Ruby", author: "Russ Olsen" },
+  { name: "The Ruby Programming Language", author: "David Flanagan" }
 ]
 
-eloquent = Book.find_by name: "Elouquent Ruby"
+100.times { |index| Book.create! name: "Book#{index}", author: "Author#{index}" }
+
+eloquent = Book.find_by name: "Eloquent Ruby"
 eloquent.notes.create! [
-	{ title: "Wow", note: "Great book to learn Ruby" },
-	{ title: "Funny", note: "Doesn't put you to sleep" }
+  { title: "Wow", note: "Great book to learn Ruby" },
+  { title: "Funny", note: "Doesn't put you to sleep" }
 ]
 
 reviewers = Reviewer.create! [
-	{ name: "Joe", password: "abc123" },
-	{ name: "Jim", password: "123abc" }
+  { name: "Joe", password: "abc123" },
+  { name: "Jim", password: "123abc" }
 ]
 
 Book.all.each do |book|
-	book.reviewer = reviewers.sample
-	book.save!
+  book.reviewer = reviewers.sample
+  # assigning reviewers randomly to each book
+  book.save!
 end
