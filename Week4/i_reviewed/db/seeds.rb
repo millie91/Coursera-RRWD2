254e9771e6472cc6cfc74ad51ad096d86a2889d6
Milica Sucevic <milicasucevic@gmail.com>
2017-02-25 10:18:41 +0000

    Scaffold_controller generated

--- Week4/i_reviewed/db/seeds.rb
+++ Week4/i_reviewed/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Book.destroy_all
+
+Book.create! [
+ { name: "Elouquent Ruby", author: "Russ Olsen" },
+ { name: "Beginning Ruby", author: "Peter Cooper" },
+ { name: "Metaprogramming Ruby 2", author: "Paolo Perotta" },
+ { name: "Design Patterns in Ruby", author: "Russ Olsen" },
+ { name: "The Ruby Programming Language", author: "David Flanagan" },
+]
