a40610146455e6dc7e9a84dbef2c5a140e88e469
Milica Sucevic <milicasucevic@gmail.com>
2017-02-27 19:22:14 +0000

    Code Updated

--- Week4/todolists/db/schema.rb
+++ Week4/todolists/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170212123959) do
+ActiveRecord::Schema.define(version: 20170227190841) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
@@ -40,9 +40,9 @@ ActiveRecord::Schema.define(version: 20170212123959) do
   create_table "todo_lists", force: :cascade do |t|
     t.string   "list_name"
     t.date     "list_due_date"
-    t.integer  "user_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "user_id"
   end
 
   add_index "todo_lists", ["user_id"], name: "index_todo_lists_on_user_id"
